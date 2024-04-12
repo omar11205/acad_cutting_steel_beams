f6db02ee98474ccccb2824aa7030f8541c799dc6
Omar <omarferbol@gmail.com>
2024-04-11 20:38:06 -0500

    Agregar archivos de proyecto.

---
+++ autocad_first_pluggin.cs
@@ -0,0 +1,24 @@
+﻿using autocad_plugin_first;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.EditorInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly:CommandClass(typeof(autocad_plugin_first.autocad_first_pluggin))]
+namespace autocad_plugin_first
+{
+    public class autocad_first_pluggin
+    {
+        [CommandMethod("Hello")]
+        public void HelloWorld() {
+            Document doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            
+            ed.WriteMessage("Hello Fucking World!! /,,/");
+        }
+    }
+}
